360701499b86c908f3fbdb5a3a9518b911e5283e
Dillon Ellis <dillonzellis@gmail.com>
2023-01-28 12:28:58 -0500

    intro component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Intro from "./components/Intro";
+
+function App() {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className=" flex flex-col items-center justify-center pt-20 pb-6 text-center">
+      <h1 className="mb-1 text-4xl font-bold md:mb-3 md:text-7xl">
+        Dillon Ellis
+      </h1>
+      <div className="mb-3 text-base font-medium md:text-xl">Web Developer</div>
+      <p className="mb-6 max-w-xl text-sm font-bold">
+        Hard-working, motivated individual. Well versed in various computer
+        languages focusing web development with a background in empirical
+        research studies from design to analyzing data. Possesses a Bachelor of
+        Science, impeccable written and verbal communication skills, and
+        excellent interpersonal skills.
+      </p>
+    </div>
+  );
+}
+
+export default Intro;
